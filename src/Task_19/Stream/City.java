1aad1cbfbff9eb01ccc942b0f769e0b3f7fe5343
Bogdan <seredyuk.b@gmail.com>
2025-12-04 01:09:02 +0300

    Task 18

---
+++ src/Task_19/Stream/City.java
@@ -0,0 +1,33 @@
+package Task_19.Stream;
+
+public class City {
+
+    private int id;
+    private String name;
+    private int population;
+    private Continent continent;
+
+    public City(int id, String name, int population, Continent continent) {
+        this.id = id;
+        this.name = name;
+        this.population = population;
+        this.continent = continent;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+
+    public Continent getContinent() {
+        return continent;
+    }
+}
---
+++ src/Task_19/StringProcessor.java
@@ -0,0 +1,6 @@
+package Task_19;
+
+public interface StringProcessor {
+    String process(String input);
+
+}
